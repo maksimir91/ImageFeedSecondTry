97d0df3ca7f6bcbdde9c716c3c29f951e6ac0ea1
maksimir91 <maksimirrus@gmail.com>
2024-05-06 16:43:17 +0300

    Initial Commit

---
+++ ImageFeedSecondTry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeedSecondTry
+//
+//  Created by Stanislav Shut on 06.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
